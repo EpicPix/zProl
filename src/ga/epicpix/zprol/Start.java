56e1826360c578acc612c01b06c00ff144d5e84a
EpicPix <epicpixminecraft@gmail.com>
2021-10-19 21:22:14 +0200

    Add basic file checks in arguments

--- src/ga/epicpix/zprol/Start.java
+++ src/ga/epicpix/zprol/Start.java
@@ -1,7 +1,20 @@
 package ga.epicpix.zprol;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Start {
 
-
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0) {
+            throw new IllegalArgumentException("File not specified");
+        }
+        String fileName = String.join(" ", args);
+        File file = new File(fileName);
+        if(!file.exists()) {
+            throw new FileNotFoundException(fileName);
+        }
+    }
 
 }
