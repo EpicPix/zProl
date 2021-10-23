d478fb8a5edc36216a3b2e01772df40af1ad8a17
EpicPix <epicpixminecraft@gmail.com>
2021-10-23 12:42:05 +0200

    Fix TypeFutureStructure using Types.FUTURE_OBJECT

--- src/ga/epicpix/zprol/compiled/TypeFutureStructure.java
+++ src/ga/epicpix/zprol/compiled/TypeFutureStructure.java
@@ -5,7 +5,7 @@ public class TypeFutureStructure extends Type {
     public final String type;
 
     public TypeFutureStructure(String type) {
-        super(Types.FUTURE_OBJECT);
+        super(Types.FUTURE_STRUCTURE);
         this.type = type;
     }
 
