bc52d8fd4eaf81e533fd11a5e492668573a8efa6
EpicPix <epicpixminecraft@gmail.com>
2021-10-29 21:37:50 +0200

    Add more instructions that can get converted

--- src/ga/epicpix/zprol/Generator.java
+++ src/ga/epicpix/zprol/Generator.java
@@ -32,6 +32,7 @@ public class Generator {
                 Bytecode bc = func.code;
                 writer.write("    enter " + bc.getLocalVariablesSize() + ", 0\n");
                 for(BytecodeInstruction instr : bc.getInstructions()) {
+                    writer.write("    ; " + instr.instruction.name().toLowerCase() + "\n");
                     if(instr.instruction == BytecodeInstructions.PUSHI8) {
                         writer.write("    dec rsp\n");
                         writer.write("    mov byte [rsp], " + instr.data[0] + "\n");
@@ -57,10 +58,39 @@ public class Generator {
                     }else if(instr.instruction == BytecodeInstructions.STORE64) {
                         writer.write("    pop rax\n");
                         writer.write("    mov qword [rbp-" + instr.data[0] + "], rax\n");
+                    }else if(instr.instruction == BytecodeInstructions.LOAD8) {
+                        writer.write("    mov byte al, [rbp-" + instr.data[0] + "],\n");
+                        writer.write("    dec rsp\n");
+                        writer.write("    mov byte [rsp], al\n");
+                    }else if(instr.instruction == BytecodeInstructions.LOAD16) {
+                        writer.write("    mov word ax, [rbp-" + instr.data[0] + "],\n");
+                        writer.write("    push ax\n");
+                    }else if(instr.instruction == BytecodeInstructions.LOAD32) {
+                        writer.write("    mov dword eax, [rbp-" + instr.data[0] + "],\n");
+                        writer.write("    sub rsp, 4\n");
+                        writer.write("    mov dword [rsp], eax\n");
+                    }else if(instr.instruction == BytecodeInstructions.LOAD64) {
+                        writer.write("    mov qword rax, [rbp-" + instr.data[0] + "],\n");
+                        writer.write("    sub rsp, 8\n");
+                        writer.write("    mov qword [rsp], rax\n");
+                    }else if(instr.instruction == BytecodeInstructions.ADD8) {
+                        writer.write("    mov byte al, [rsp]\n");
+                        writer.write("    inc rsp\n");
+                        writer.write("    mov byte ah, [rsp],\n");
+                        writer.write("    inc rsp\n");
+                        writer.write("    add al, ah\n");
+                        writer.write("    dec rsp\n");
+                        writer.write("    mov byte [rsp], ah\n");
+                    }else if(instr.instruction == BytecodeInstructions.ADD16) {
+                        writer.write("    pop ax\n");
+                        writer.write("    pop bx\n");
+                        writer.write("    add ax, bx\n");
+                        writer.write("    push ax\n");
                     }else {
                         System.err.println("Missing instruction: " + instr);
                     }
                 }
+                writer.write("    ; exit\n");
                 writer.write("    mov rax, 60\n");
                 writer.write("    mov rdi, 0\n");
                 writer.write("    syscall\n");
