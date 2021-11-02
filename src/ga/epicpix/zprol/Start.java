e0586496d3f39aa6a276e4f929d4148dedfa96f8
EpicPix <epicpixminecraft@gmail.com>
2021-11-02 16:55:07 +0100

    Create porth generator

--- src/ga/epicpix/zprol/Start.java
+++ src/ga/epicpix/zprol/Start.java
@@ -3,6 +3,7 @@ package ga.epicpix.zprol;
 import ga.epicpix.zprol.compiled.CompiledData;
 import ga.epicpix.zprol.exceptions.UnknownTypeException;
 import ga.epicpix.zprol.generators.GeneratorAssembly;
+import ga.epicpix.zprol.generators.GeneratorPorth;
 import ga.epicpix.zprol.tokens.Token;
 import java.io.File;
 import java.io.IOException;
@@ -26,9 +27,16 @@ public class Start {
         compiled.save(new File(normalName + ".zpil"));
         long startConvert = System.currentTimeMillis();
         System.out.printf("Took %d ms to save\n", startConvert - startSave);
+
+        long gen_x86_64_linux_asm_start = System.currentTimeMillis();
         GeneratorAssembly.generate_x86_64_linux_assembly(compiled, new File(normalName + "_64linux.asm"));
-        long end = System.currentTimeMillis();
-        System.out.printf("Took %d ms to generate x86-64 linux assembly\n", end - startConvert);
+        long gen_x86_64_linux_asm_end = System.currentTimeMillis();
+        System.out.printf("Took %d ms to generate x86-64 linux assembly\n", gen_x86_64_linux_asm_end - gen_x86_64_linux_asm_start);
+
+        long gen_porth_start = System.currentTimeMillis();
+        GeneratorPorth.generate_porth(compiled, new File(normalName + ".porth"));
+        long gen_porth_end = System.currentTimeMillis();
+        System.out.printf("Took %d ms to generate porth\n", gen_porth_end - gen_porth_start);
     }
 
 }
