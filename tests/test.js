8f9b144aafcf4848b9c8b764412fa398b1a2383a
EpicPix <epicpixminecraft@gmail.com>
2021-12-21 19:25:50 +0100

    Update testing

--- tests/test.js
+++ tests/test.js
@@ -5,20 +5,30 @@ const { spawn } = require('child_process');
 const outputs = JSON.parse(fs.readFileSync('.outputs.json').toString());
 
 async function main() {
-    var failed = false;
+    var fails = [];
     for(const file of fs.readdirSync('.')) {
         if(file.endsWith('.zprol')) {
-            const process = spawn('java', ['-jar', '../zProL.jar', file]);
+            var nfile = file.substring(0, file.length - ".zprol".length);
+            console.log(`----------${nfile}----------`);
+            console.log(file);
+            const process = spawn('java', ['-jar', '../zProL.jar', file], {stdio: ['ignore', 'pipe', 'pipe']});
+            process.stdout.setEncoding('utf8');
+            var data = "";
+            process.stdout.on('data', (chunk) => data += chunk).on("error", () => {});
+            process.stderr.on('data', (chunk) => data += chunk).on("error", () => {});
             const finish = await new Promise((resolve) => process.on('close', resolve));
-            const f = file.substring(0, file.length - ".zprol".length);
-            const out = outputs[f];
+            const out = outputs[nfile];
             if(out.exit != finish) {
-                failed = true;
-                console.error(`Test failed for ${file}`);
+                console.error(data);
+                console.error(`Test failed`);
+                fails.push(file);
+            }else {
+                console.log(`Test succeeded`);
             }
         }
     }
-    if(failed) {
+    if(fails) {
+        console.error(fails.map((file) => `Test failed for ${file}`).join("\n"));
         process.exit(1);
     }
 }
