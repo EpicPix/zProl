18ba54e71ccaca1931c3599676cf5f8042369e8f
EpicPix <epicpixminecraft@gmail.com>
2022-06-26 18:55:38 +0200

    Add attributes

---
+++ zpil/src/main/java/ga/epicpix/zpil/attr/Attribute.java
@@ -0,0 +1,52 @@
+package ga.epicpix.zpil.attr;
+
+import ga.epicpix.zpil.pool.ConstantPool;
+import ga.epicpix.zpil.pool.ConstantPoolEntry;
+
+import java.io.*;
+
+public abstract class Attribute {
+
+    private final String name;
+
+    public Attribute(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Attribute read(DataInput input, ConstantPool pool) throws IOException {
+        String name = ((ConstantPoolEntry.StringEntry) pool.entries.get(input.readInt())).getString();
+        int length = input.readInt();
+        byte[] data = new byte[length];
+        for(int i = 0; i<length; i++) data[i] = input.readByte();
+        Attribute inst = switch(name) {
+            case "ConstantValue" -> new ConstantValueAttribute();
+            default -> null;
+        };
+        if(inst == null) return null;
+        inst.readData(new DataInputStream(new ByteArrayInputStream(data)), pool);
+        return inst;
+    }
+
+    public void prepareConstantPool(ConstantPool pool) {
+        pool.getOrCreateStringIndex(name);
+    }
+
+    public final byte[] write(ConstantPool pool) throws IOException {
+        var arr = new ByteArrayOutputStream();
+        var out = new DataOutputStream(arr);
+        out.writeInt(pool.getStringIndex(name));
+        var data = new ByteArrayOutputStream();
+        writeData(new DataOutputStream(data), pool);
+        out.writeInt(data.size());
+        out.write(data.toByteArray());
+        return arr.toByteArray();
+    }
+
+    protected abstract void readData(DataInput input, ConstantPool pool) throws IOException;
+    protected abstract void writeData(DataOutputStream out, ConstantPool pool) throws IOException;
+
+}
---
+++ zpil/src/main/java/ga/epicpix/zpil/attr/ConstantValueAttribute.java
@@ -0,0 +1,78 @@
+package ga.epicpix.zpil.attr;
+
+import ga.epicpix.zpil.pool.ConstantPool;
+import ga.epicpix.zpil.pool.ConstantPoolEntry;
+
+import java.io.DataInput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class ConstantValueAttribute extends Attribute {
+
+    private Object value;
+
+    private ConstantValueAttribute(Object value){
+        super("ConstantValue");
+        this.value = value;
+    }
+
+    public ConstantValueAttribute()   { this(null); }
+    public ConstantValueAttribute(boolean value) { this((Object) value); }
+    public ConstantValueAttribute(byte value)    { this((Object) value); }
+    public ConstantValueAttribute(short value)   { this((Object) value); }
+    public ConstantValueAttribute(int value)     { this((Object) value); }
+    public ConstantValueAttribute(long value)    { this((Object) value); }
+    public ConstantValueAttribute(String value)  { this((Object) value); }
+
+    public Object getValue() {
+        return value;
+    }
+
+    protected void readData(DataInput input, ConstantPool pool) throws IOException {
+        int type = input.readUnsignedByte();
+        switch(type) {
+            case 'n' -> value = null;
+            case 's' -> value = ((ConstantPoolEntry.StringEntry) pool.entries.get(input.readInt())).getString();
+            case 'b' -> value = input.readBoolean();
+            case 'B' -> value = input.readByte();
+            case 'S' -> value = input.readShort();
+            case 'I' -> value = input.readInt();
+            case 'L' -> value = input.readLong();
+            default -> throw new IllegalStateException("Unknown type of constant value '" + type + "'");
+        }
+    }
+
+    public void prepareConstantPool(ConstantPool pool) {
+        super.prepareConstantPool(pool);
+        if(value instanceof String str) {
+            pool.getOrCreateStringIndex(str);
+        }
+    }
+
+    protected void writeData(DataOutputStream out, ConstantPool pool) throws IOException {
+        if(value == null) {
+            out.writeByte('n');
+        }else if(value instanceof String str) {
+            out.writeByte('s');
+            out.writeInt(pool.getStringIndex(str));
+        }else if(value instanceof Boolean v) {
+            out.writeByte('b');
+            out.writeBoolean(v);
+        }else if(value instanceof Byte v) {
+            out.writeByte('B');
+            out.writeByte(v);
+        }else if(value instanceof Short v) {
+            out.writeByte('S');
+            out.writeShort(v);
+        }else if(value instanceof Integer v) {
+            out.writeByte('I');
+            out.writeInt(v);
+        }else if(value instanceof Long v) {
+            out.writeByte('L');
+            out.writeLong(v);
+        }else {
+            throw new IllegalStateException("Unknown type of constant value '" + value.getClass().getSimpleName() + "'");
+        }
+    }
+
+}
